03ee7204c026ab5bab580c5d219caa5a64366693
hhs0001 <heitorstein@gmail.com>
2022-11-14 20:13:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+
+# Este projeto visa a criação de uma rede de compartilhamento de arquivos e uma impressora de pdfs virtual
+
+Seguindo as intruções dadas pelo professor Martins:
+
+```
+Samba e CUPS:
+1. Instalação e configuração do serviço de arquivos em rede (Samba), com:
+a) Criação de um grupo de trabalho REDES2022;
+b) Compartilhamento de um diretório “arquivos” e 2 usuários (com login e senha),
+sendo 1 com direito de escrita e leitura e 1 apenas leitura.
+2. Instalação e configuração do serviço de impressão (CUPS), com:
+a) Instalar e configurar uma impressora PDF no CUPS;
+b) Configurar o compartilhamento em rede da impressora via Samba (não se
+esqueça do driver para os clientes).
+```
+## Instalação
+
+Para dar inicio a Instalação primeiramente clone o projeto
+
+```bash
+  git clone https://github.com/hhs0001/installcupssamba
+```
+
+Depois basta mudar o diretório e dar inicio a Instalação
+
+```bash
+  cd installcupssamba
+```
+```bash
+  chmod +x install.sh
+``` 
+```bash
+  ./install.sh
+``` 
+## Autores
+
+- [@hhs0001](https://www.github.com/hhs0001)
+- [@autor2](https://www.github.com/autor2)
+- [@autor3](https://www.github.com/autor3)
+- [@autor4](https://www.github.com/autor4)
+
---
+++ install.sh
@@ -0,0 +1,81 @@
+echo "Este script instala o cups e o samba para compartilhar a impressora pdf e com seus respectivos drivers e grupos"
+
+echo "aperte qualquer tecla para continuar para continuar"
+
+read
+
+#checa se o script esta sendo executado como root e se nao estiver ele envia uma mensagem de erro e sai do script
+
+if [ "$(whoami)" != "root" ]; then
+	echo "Este script precisa ser executado como root"
+	exit
+fi
+
+#update pacotes
+
+apt-get update
+
+# Instala o cups e o samba
+
+apt-get install cups samba
+
+# Cria o grupo de trabalho REDES2022
+
+groupadd REDES2022
+
+#criar o diretorio arquivos no /home e compartilha-lo com o grupo redes2022 com as permissoes de leitura e escrita
+
+mkdir /home/arquivos
+
+chmod 777 /home/arquivos
+
+chown root:REDES2022 /home/arquivos
+
+# Cria o usuario "usuario1" e adiciona-o ao grupo redes2022
+
+adduser --system usuario1
+
+usermod -a -G REDES2022 usuario1
+
+# Cria o usuario "usuario2" e adiciona-o ao grupo redes2022
+
+adduser --system usuario2
+
+usermod -a -G REDES2022 usuario2
+
+# inicia o samaba
+
+systemctl start smb
+
+echo "digite a senha do usuario1"
+
+smbpasswd -a usuario1
+
+echo "digite a senha do usuario2"
+
+smbpasswd -a usuario2
+
+chown usuario1:REDES2022 -R /home/arquivos
+
+chown usuario2:REDES2022 -R /home/arquivos
+
+chcon -Rt samba_share_t /home/arquivos
+
+# Cria o arquivo de configuração do samba
+
+echo "[arquivos]
+path = /home/arquivos
+valid users = usuario1, usuario2
+read list = @REDES2022
+write list = usuario1
+browseable = yes" > /etc/samba/smb.conf
+
+# cria a impressora pdf com o cups-pdf
+
+apt-get install cups-pdf
+
+# cria o arquivo de configuração do cups-pdf
+
+sudo chmod +s /usr/lib/cups/backend/cups-pdf
+
+echo "Fim do script"
